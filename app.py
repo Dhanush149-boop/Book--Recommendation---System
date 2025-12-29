9018bda839b805e6f8e197e5992bb64cfd39130b
alpha-knowledge <kanamanambedudhanush@gmail.com>
2025-12-29 23:34:53 +0530

    corrected csv file  changell

--- app.py
+++ app.py
@@ -16,15 +16,15 @@ DATA_DIR = Path(__file__).parent
 
 @st.cache_data
 def load_data():
-    required_files = ["Books.csv", "Ratings.csv", "Users.csv"]
+    required_files = ["Books", "Ratings", "Users"]
 
     for file in required_files:
         if not (DATA_DIR / file).exists():
             st.error(f"❌ Missing file: {file}")
             return None, None, None
 
-    books = pd.read_csv(DATA_DIR / "Books.csv")
-    ratings = pd.read_csv(DATA_DIR / "Ratings.csv")
-    users = pd.read_csv(DATA_DIR / "Users.csv")
+    books = pd.read_csv(DATA_DIR / "Books")
+    ratings = pd.read_csv(DATA_DIR / "Ratings")
+    users = pd.read_csv(DATA_DIR / "Users")
 
     return books, ratings, users
